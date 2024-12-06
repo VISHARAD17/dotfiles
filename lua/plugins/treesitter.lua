f9dea9e1e81467db725c087c7d869556208947c7
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-12-06 15:52:47 +0530

    update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 return
-    {   "nvim-treesitter/nvim-treesitter", 
+    {   "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
             local configs = require("nvim-treesitter.configs")
@@ -14,6 +14,13 @@ return
                 sync_install = false,
                 highlight = {
                     enable = true,
+                    disable = function(lang, buf)
+                        local max_filesize = 1000 * 1024 -- 1000 KB(1 MB)
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        end
+                    end,
                     additional_vim_regex_highlighting = true, --additional lighting using regex if available
                 },
                 indent = { enable = true },
