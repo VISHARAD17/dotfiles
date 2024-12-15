17507adfe39338993884a64fed891678cd0ce36c
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-12-15 16:30:55 +0530

    update plugins

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,6 +4,7 @@ return
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
         ---@module 'render-markdown'
         opts = {},
+        ft = "markdown",
         config = function ()
             require('render-markdown').setup({
                 render_modes = true,
