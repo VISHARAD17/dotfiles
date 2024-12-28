6a51ad79498a4b98f66b95e8607e5b66d8f6a7c3
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2024-12-28 22:08:07 +0530

    remove plugins for markdown using new plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,19 +1,11 @@
 return
     {
-        {
-            "iamcco/markdown-preview.nvim",
-            cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-            build = "cd app && yarn install",
-            init = function()
-                vim.g.mkdp_filetypes = { "markdown" }
-            end,
-            ft = { "markdown" },
-        },
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-            opts = {
-                render_modes = { 'n', 'c', 't', 'i' },
-            },
-        }
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
     }
+}
