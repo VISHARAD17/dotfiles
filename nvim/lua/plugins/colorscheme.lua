87c7da8b5e637319ba61d0c42391361a5b9eb9f3
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2025-01-12 10:23:39 +0530

    update config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -17,7 +17,7 @@ return {
         vim.api.nvim_set_hl(0, "StatusLineNC", {fg = "#ffffff", bg="#1e1e1e"})
         vim.api.nvim_set_hl(0, "NvimTreeIndentMarker", {fg ='#3f3f3f', bg = 'none' })
         vim.api.nvim_set_hl(0, "FloatBorder", {fg = '#6e7681', bg = 'none'})
-        vim.api.nvim_set_hl(0, "NvimTreeWinSeparator",{fg = '#3f3f3f', bg = 'none'})
+        vim.api.nvim_set_hl(0, "NvimTreeWinSeparator",{fg = '#a9a9a9', bg = 'none'})
         vim.api.nvim_set_hl(0, "EndOfBuffer", {fg = '#6e7681', bg = 'none'})
         -- vim.api.nvim_set_hl(0, "WinBarNC", { fg = '#a9a9a9', bg = '#181818'})
         -- vim.api.nvim_set_hl(0, "WinBar", { fg = 'none', bg = 'none' })
