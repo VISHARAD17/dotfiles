142c1ec937351c4131d0518e5a5df48f3e848add
Visharad Jaydas Borsutkar <visharadborsutkar777@gmail.com>
2025-10-26 17:27:02 +0530

    update nvim configuration

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -1,7 +1,5 @@
 require('mason').setup()
 
-
-
 vim.lsp.config['lua_ls'] = {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
@@ -64,29 +62,73 @@ vim.lsp.config['pyright'] = {
   filetypes = { 'python' },
 }
 
-
 vim.lsp.enable({ 'luals', 'clangd', 'jsonls', 'ts_ls', 'html', 'rust_analyzer', 'jdtls', 'marksman', 'eslint', 'sqls', 'pyright' })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 vim.lsp.config('*', {
-    capabilities = capabilities,
+  capabilities = capabilities,
 })
 
 -- Configure Mason to automatically install these LSP servers
 require('mason-lspconfig').setup({
-    ensure_installed = {
-        "pyright",
-        "eslint",
-        "lua_ls",
-        "sqls",
-        "cssls",
-        "jdtls",
-        "rust_analyzer",
-        "jsonls",
-        "marksman",
-    },
-    automatic_installation = true,
+  ensure_installed = {
+    "pyright",
+    "eslint",
+    "lua_ls",
+    "sqls",
+    "cssls",
+    "jdtls",
+    "rust_analyzer",
+    "jsonls",
+    "marksman",
+  },
+  automatic_installation = true,
 })
+
+------------------------------------------------------------
+-- ✨ VSCode-like LSP hover window styling (edited section)
+------------------------------------------------------------
+
+-- Custom border style like VSCode
+local border = {
+  { "╭", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╮", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "╯", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "╰", "FloatBorder" },
+  { "│", "FloatBorder" },
+}
+
+-- Make hover and signature help use rounded float with consistent look
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {
+    border = border,
+    title = "Hover Information",
+    max_width = 100,
+    max_height = 25,
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {
+    border = border,
+    title = "Signature Help",
+  }
+)
+
+vim.diagnostic.config({
+  float = { border = "rounded", source = "always" },
+})
+
+-- VSCode-like highlight colors
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e1e" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#3e3e3e", bg = "#1e1e1e" })
+
+------------------------------------------------------------
 
 -- Set up completion
 local cmp = require('cmp')
@@ -98,122 +140,110 @@ require('luasnip.loaders.from_snipmate').lazy_load()
 
 -- Set up completion
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+
+  sources = {
+    { name = 'nvim_lsp', priority = 1000 },
+    { name = 'luasnip', priority = 750 },
+    { name = 'buffer', priority = 500 },
+    { name = 'path', priority = 250 },
+    { name = 'nvim_lua', priority = 100 },
+  },
+
+  window = {
+    completion = {
+      border = "rounded",
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+      side_padding = 1,
     },
-
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    }),
-
-    sources = {
-        { name = 'nvim_lsp', priority = 1000 },  -- LSP
-        { name = 'luasnip', priority = 750 },   -- Snippets
-        { name = 'buffer', priority = 500 },    -- Text within current buffer
-        { name = 'path', priority = 250 },      -- File system paths
-        { name = 'nvim_lua', priority = 100 },  -- Neovim's Lua API
+    documentation = {
+      border = "rounded",
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
     },
+  },
 
-    window = {
-        completion = {
-            border = "rounded",
-            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
-            side_padding = 1,
-        },
-        documentation = {
-            border = "rounded",
-            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
-        },
-    },
+  formatting = {
+    fields = {"abbr", "menu", "kind"},
+    format = function(entry, vim_item)
+      local kind_icons = {
+        Text = "",
+        Method = "󰆧",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "󰇽",
+        Variable = "󰂡",
+        Class = "󰠱",
+        Interface = "",
+        Module = "",
+        Property = "󰜢",
+        Unit = "",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "󰅲",
+      }
 
-    formatting = {
-        fields = {"abbr", "menu", "kind"},
-        format = function(entry, vim_item)
-            -- Kind icons
-            local kind_icons = {
-                Text = "",
-                Method = "󰆧",
-                Function = "󰊕",
-                Constructor = "",
-                Field = "󰇽",
-                Variable = "󰂡",
-                Class = "󰠱",
-                Interface = "",
-                Module = "",
-                Property = "󰜢",
-                Unit = "",
-                Value = "󰎠",
-                Enum = "",
-                Keyword = "󰌋",
-                Snippet = "",
-                Color = "󰏘",
-                File = "󰈙",
-                Reference = "",
-                Folder = "󰉋",
-                EnumMember = "",
-                Constant = "󰏿",
-                Struct = "",
-                Event = "",
-                Operator = "󰆕",
-                TypeParameter = "󰅲",
-            }
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+        nvim_lua = "[Lua]",
+      })[entry.source.name]
+      return vim_item
+    end
+  },
 
-            -- Kind text and icons
-            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
-
-            -- Source
-            vim_item.menu = ({
-                nvim_lsp = "[LSP]",
-                luasnip = "[Snippet]",
-                buffer = "[Buffer]",
-                path = "[Path]",
-                nvim_lua = "[Lua]",
-            })[entry.source.name]
-
-            return vim_item
-        end
-    },
-
-    experimental = {
-        ghost_text = false,  -- Show future text as gray text
-    }
+  experimental = {
+    ghost_text = false,
+  }
 })
 
 -- Add LSP specific keymaps
 vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        -- You can configure your specific LSP keymaps here
-        -- or use whichkey.lua to set them up globally
-        
-        -- Examples of buffer-local keymaps
-        -- local opts = { buffer = ev.buf }
-        -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    end,
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- keymaps can go here
+  end,
 })
